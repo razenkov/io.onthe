645ed0f72f4d6b7921884bc80cf54b40d892c2d7
vrazenkov <razenkov.slava@gmail.com>
2019-12-20 22:12:26 +0200

    Jenkins test2

--- src/test/java/com/googleTest/SearchTest.java
+++ src/test/java/com/googleTest/SearchTest.java
@@ -1,10 +1,11 @@
 package com.googleTest;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchTest {
     @Test
     public void searchTest(){
-
+        Assert.assertTrue(true);
     }
 }
